565d9ea81e1715557371d08e39d220b87ea60ef5
Sergejs Kuzema <s.kuzema@gmail.com>
2024-06-16 18:03:00 +0300

    init: Task 3

---
+++ test/aws-backend.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsBackend from '../lib/aws-backend-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-backend-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsBackend.AwsBackendStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
