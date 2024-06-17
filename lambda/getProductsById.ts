5f968742d1d316804d99cd46c8f6c3940f0a244f
Sergejs Kuzema <s.kuzema@gmail.com>
2024-06-17 15:59:19 +0300

    feat: add products

--- lambda/getProductsById.ts
+++ lambda/getProductsById.ts
@@ -1,9 +1,17 @@
-import { Handler } from "aws-lambda";
+import { Handler, APIGatewayProxyResult } from "aws-lambda";
 
-export const handler: Handler = async (event: any = {}): Promise<any> => {
+export const handler: Handler = async (
+  event: any = {}
+): Promise<APIGatewayProxyResult> => {
   const products = [
     { id: "1", name: "Product 1", price: 100 },
-    { id: "2", name: "Product 2", price: 200 },
+    { id: "2", name: "Product 2", price: 150 },
+    { id: "3", name: "Product 3", price: 200 },
+    { id: "4", name: "Product 4", price: 250 },
+    { id: "5", name: "Product 5", price: 300 },
+    { id: "6", name: "Product 6", price: 350 },
+    { id: "7", name: "Product 7", price: 400 },
+    { id: "8", name: "Product 8", price: 450 },
   ];
 
   const productId = event.pathParameters.productId;
@@ -12,12 +20,22 @@ export const handler: Handler = async (event: any = {}): Promise<any> => {
   if (!product) {
     return {
       statusCode: 404,
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET",
+      },
       body: JSON.stringify({ message: "Product not found" }),
     };
   }
 
   return {
     statusCode: 200,
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET",
+    },
     body: JSON.stringify(product),
   };
 };
--- lambda/getProductsList.ts
+++ lambda/getProductsList.ts
@@ -1,14 +1,26 @@
-import { Handler } from "aws-lambda";
+import { Handler, APIGatewayProxyResult } from "aws-lambda";
 
-export const handler: Handler = async (event: any = {}): Promise<any> => {
+export const handler: Handler = async (
+  event: any = {}
+): Promise<APIGatewayProxyResult> => {
   const products = [
     { id: "1", name: "Product 1", price: 100 },
-    { id: "2", name: "Product 2", price: 200 },
+    { id: "2", name: "Product 2", price: 150 },
+    { id: "3", name: "Product 3", price: 200 },
+    { id: "4", name: "Product 4", price: 250 },
+    { id: "5", name: "Product 5", price: 300 },
+    { id: "6", name: "Product 6", price: 350 },
+    { id: "7", name: "Product 7", price: 400 },
+    { id: "8", name: "Product 8", price: 450 },
   ];
 
   return {
     statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Methods": "GET",
+    },
     body: JSON.stringify(products),
-    isBase64Encoded: false,
   };
 };
