68a988bffa459e29fea0dbd131a32a40c2f32664
Sergejs Kuzema <s.kuzema@gmail.com>
2024-06-17 18:51:36 +0300

    feat: api

---
+++ lambda/mock_products.ts
@@ -0,0 +1,57 @@
+export type Product = {
+  id: string;
+  title: string;
+  description: string;
+  price: number;
+};
+
+export const products: Product[] = [
+  {
+    description: "Short Product Description1",
+    id: "7567ec4b-b10c-48c5-9345-fc73c48a80aa",
+    price: 24,
+    title: "ProductOne",
+  },
+  {
+    description: "Short Product Description2",
+    id: "7567ec4b-b10c-48c5-9345-fc73c48a80ab",
+    price: 30,
+    title: "ProductTwo",
+  },
+  {
+    description: "Short Product Description3",
+    id: "7567ec4b-b10c-48c5-9345-fc73c48a80ac",
+    price: 45,
+    title: "ProductThree",
+  },
+  {
+    description: "Short Product Description4",
+    id: "7567ec4b-b10c-48c5-9345-fc73c48a80ad",
+    price: 60,
+    title: "ProductFour",
+  },
+  {
+    description: "Short Product Description5",
+    id: "7567ec4b-b10c-48c5-9345-fc73c48a80ae",
+    price: 22,
+    title: "ProductFive",
+  },
+  {
+    description: "Short Product Description6",
+    id: "7567ec4b-b10c-48c5-9345-fc73c48a80af",
+    price: 35,
+    title: "ProductSix",
+  },
+  {
+    description: "Short Product Description7",
+    id: "7567ec4b-b10c-48c5-9345-fc73c48a80b0",
+    price: 50,
+    title: "ProductSeven",
+  },
+  {
+    description: "Short Product Description8",
+    id: "7567ec4b-b10c-48c5-9345-fc73c48a80b1",
+    price: 28,
+    title: "ProductEight",
+  },
+];
