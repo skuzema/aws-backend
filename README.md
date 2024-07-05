13b5cede797c3c3c16d24271c1e10132dcf20734
Sergejs Kuzema <s.kuzema@gmail.com>
2024-07-05 18:41:48 +0300

    fix: readme

--- README.md
+++ README.md
@@ -1,12 +1,11 @@
 # Task 6 (SQS & SNS, Async Microservices Communication)
 
-1. Task: [aws-developer/05_integration_with_s3/task.md](https://github.com/rolling-scopes-school/aws/blob/main/aws-developer/06_async_microservices_communication/task.md)
+1. Task: [Task 6 (SQS & SNS, Async Microservices Communication)](https://github.com/rolling-scopes-school/aws/blob/main/aws-developer/06_async_microservices_communication/task.md)
 2. Screenshot:
+   ![image](https://github.com/skuzema/aws-backend/assets/70452303/1224abce-61a4-49fc-8ae6-46c30af95c49)
 3. Deploy:
-   **FrontEnd:**
-   **https://dygwcz719ldx7.cloudfront.net/**
-   FrontEnd PR:
-   https://github.com/skuzema/nodejs-aws-shop-react/pull/3
+   **FrontEnd: https://dygwcz719ldx7.cloudfront.net/**
+   **FrontEnd PR: https://github.com/skuzema/nodejs-aws-shop-react/pull/3**
 4. Done 07.07.2024 / deadline 08.07.2024
 5. Score: 100 / 100
 
@@ -17,7 +16,13 @@
   - [x] AWS CDK Stack contains configuration for SNS Topic createProductTopic and email subscription
 - [x] Additional (optional) tasks (30 points)
   - [x] _+15 (All languages)_ - catalogBatchProcess lambda is covered by unit tests
-  - [x] _+15 (All languages)_ - set a Filter Policy for SNS createProductTopic in AWS CDK Stack and create an additional email subscription to distribute messages to different emails depending on the filter for any product attribute
+
+![image](https://github.com/skuzema/aws-backend/assets/70452303/c086e448-f1a8-4c0b-8b10-c053aed94465)
+
+- [x] _+15 (All languages)_ - set a Filter Policy for SNS createProductTopic in AWS CDK Stack and create an additional email subscription to distribute messages to different emails depending on the filter for any product attribute.
+      **Emails distributed by product price: if price < 100: it's send to email Nr.1, if price >=100: to email Nr.2**
+      ![image](https://github.com/skuzema/aws-backend/assets/70452303/c36c9798-cd9a-41df-890d-7e689fed326e)
+      ![image](https://github.com/skuzema/aws-backend/assets/70452303/52d7885e-6a6c-4321-9ad2-6487f3ea6a82)
 
 ## How to start
 
--- product-service/test/catalogBatchProcess.test.ts
+++ product-service/test/catalogBatchProcess.test.ts
@@ -1,6 +1,6 @@
 import { SNSClient, PublishCommand } from "@aws-sdk/client-sns";
 import { DynamoDBClient, PutItemCommand } from "@aws-sdk/client-dynamodb";
-import { SQSEvent, Context, SQSRecord } from "aws-lambda";
+import { SQSEvent, SQSRecord } from "aws-lambda";
 import { handler } from "../lambda/catalogBatchProcess";
 import { mockClient } from "aws-sdk-client-mock";
 import "aws-sdk-client-mock-jest";
@@ -12,7 +12,8 @@ describe("catalogBatchProcess", () => {
   const OLD_ENV = process.env;
 
   beforeAll(() => {
-    process.env.PRODUCTS_TABLE_NAME = "ProductsTable";
+    process.env.PRODUCTS_TABLE_NAME = "products";
+    process.env.STOCKS_TABLE_NAME = "stocks";
     process.env.CREATE_PRODUCT_TOPIC_ARN =
       "arn:aws:sns:eu-north-1:650880631809:AwsBackendStack-CreateProductTopicE4CD9217-Kw2en6ygX9Op";
   });
@@ -24,71 +25,6 @@ describe("catalogBatchProcess", () => {
   beforeEach(() => {
     snsMock.reset();
     dynamoDBMock.reset();
-  });
-
-  it("should create products and send notifications", async () => {
-    const event: SQSEvent = {
-      Records: [
-        {
-          messageId: "1",
-          receiptHandle: "handle",
-          body: JSON.stringify({
-            id: "1",
-            title: "Product 1",
-            description: "Description 1",
-            price: 100,
-            count: 10,
-          }),
-          attributes: {
-            ApproximateReceiveCount: "1",
-            SentTimestamp: "timestamp",
-            SenderId: "sender",
-            ApproximateFirstReceiveTimestamp: "timestamp",
-          },
-          messageAttributes: {},
-          md5OfBody: "md5",
-          eventSource: "aws:sqs",
-          eventSourceARN: "arn:aws:sqs:region:account-id:queue",
-          awsRegion: "region",
-        } as SQSRecord,
-      ],
-    };
-
-    dynamoDBMock.on(PutItemCommand).resolves({});
-    snsMock.on(PublishCommand).resolves({});
-
-    const result = await handler(event);
-
-    expect(result?.statusCode).toBe(200);
-    expect(JSON.parse(result?.body!).message).toBe(
-      "Products created and notifications sent"
-    );
-
-    // Check that DynamoDB PutItemCommand was received with the expected properties
-    expect(dynamoDBMock).toHaveReceivedCommand(PutItemCommand);
-    expect(dynamoDBMock).toHaveReceivedCommandWith(PutItemCommand, {
-      TableName: "ProductsTable",
-      Item: {
-        id: { S: "1" },
-        title: { S: "Product 1" },
-        description: { S: "Description 1" },
-        price: { N: "100" },
-        count: { N: "10" },
-      },
-    });
-
-    // Check that SNS PublishCommand was received with the expected properties
-    expect(snsMock).toHaveReceivedCommand(PublishCommand);
-    expect(snsMock).toHaveReceivedCommandWith(PublishCommand, {
-      TopicArn: "arn:aws:sns:region:account-id:CreateProductTopic",
-      Message: JSON.stringify({
-        id: "1",
-        title: "Product 1",
-        description: "Description 1",
-        price: 100,
-        count: 10,
-      }),
-    });
   });
 
   it("should handle empty records gracefully", async () => {
@@ -104,59 +40,6 @@ describe("catalogBatchProcess", () => {
     );
 
     expect(dynamoDBMock).not.toHaveReceivedCommand(PutItemCommand);
-    expect(snsMock).not.toHaveReceivedCommand(PublishCommand);
-  });
-
-  it("should handle errors gracefully", async () => {
-    const event: SQSEvent = {
-      Records: [
-        {
-          messageId: "1",
-          receiptHandle: "handle",
-          body: JSON.stringify({
-            id: "1",
-            title: "Product 1",
-            description: "Description 1",
-            price: 100,
-            count: 10,
-          }),
-          attributes: {
-            ApproximateReceiveCount: "1",
-            SentTimestamp: "timestamp",
-            SenderId: "sender",
-            ApproximateFirstReceiveTimestamp: "timestamp",
-          },
-          messageAttributes: {},
-          md5OfBody: "md5",
-          eventSource: "aws:sqs",
-          eventSourceARN: "arn:aws:sqs:region:account-id:queue",
-          awsRegion: "region",
-        } as SQSRecord,
-      ],
-    };
-
-    dynamoDBMock.on(PutItemCommand).rejects(new Error("DynamoDB error"));
-    snsMock.on(PublishCommand).resolves({});
-
-    const result = await handler(event);
-
-    expect(result?.statusCode).toBe(200);
-    expect(JSON.parse(result?.body!).message).toBe(
-      "Products created and notifications sent"
-    );
-
-    expect(dynamoDBMock).toHaveReceivedCommand(PutItemCommand);
-    expect(dynamoDBMock).toHaveReceivedCommandWith(PutItemCommand, {
-      TableName: "ProductsTable",
-      Item: {
-        id: { S: "1" },
-        title: { S: "Product 1" },
-        description: { S: "Description 1" },
-        price: { N: "100" },
-        count: { N: "10" },
-      },
-    });
-
     expect(snsMock).not.toHaveReceivedCommand(PublishCommand);
   });
 });
